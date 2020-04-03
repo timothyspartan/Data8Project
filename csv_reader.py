2164860700038a421442046b55357a0892fe71b3
Kate Greengrass <kgreengrass@spartablobal.com>
2020-04-03 14:49:37 +0100

    Fixed the csv reader to included talent as an option, now packaged nicely into a fucntion

--- csv_reader.py
+++ csv_reader.py
@@ -1,12 +1,14 @@
-##not nicely packaged as a fucntion yet sorry
+
 import boto3
 from pprint import pprint
 
-s3_client = boto3.client('s3')
-contents = s3_client.list_objects_v2(Bucket='data8-engineering-project')['Contents']
-for x in contents:
-    splitfile = x['Key'].split('/')
-    if splitfile[0] == 'Academy':   ###doesnt work because all the talent ones are more than 1000th in the list
-        data = s3_client.get_object(Bucket='data8-engineering-project',
-                                    Key = x['Key'])
-        pprint(data['Body'].read())
+# #####for csvs  ##simply add the folder name of the folder of csvs you want to access
+def csv_reader(folder):
+    s3_client = boto3.client('s3')
+    contents = s3_client.list_objects_v2(Bucket='data8-engineering-project', Prefix= folder + '/')['Contents']
+    for x in contents:
+        splitfile = x['Key'].split('/')
+        if splitfile[0] == folder:   ###doesnt work because all the talent ones are more than 1000th in the list
+            data = s3_client.get_object(Bucket='data8-engineering-project',
+                                        Key = x['Key'])
+            pprint(data['Body'].read())
